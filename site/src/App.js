1555a74b34bacf31172e781336c4ee25fa1e4443
Lesenmiao "David" Yu <dyu18149@usc.edu>
2024-03-07 21:25:27 -0800

    Fixed up Routing and added some test

--- site/src/App.js
+++ site/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 import LogIn from "./pages/LogIn/LogIn";
-//import Search from "./pages/Search/Search";
+import Search from "./pages/Search/Search"
 
 function App(){
     return (
@@ -10,7 +10,7 @@ function App(){
             <Routes>
                 <Route path="/" element={<LogIn />} />
                 <Route path="*" element={<Navigate to="/" replace />} />
-                {/*<Route path="/search" element={<Search />} />*/}
+                <Route path="/search" element={<Search />} />
             </Routes>
         </div>
     )
