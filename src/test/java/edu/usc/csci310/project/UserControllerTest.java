dc6935656d9e010dbb9e599f81aca7589ba70e79
Xingxin Guo <guoxingx@usc.edu>
2024-03-28 21:56:54 -0700

    Feature 1 & Feature 2

--- src/test/java/edu/usc/csci310/project/UserControllerTest.java
+++ src/test/java/edu/usc/csci310/project/UserControllerTest.java
@@ -14,6 +14,7 @@ import static org.hamcrest.core.StringContains.containsString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
